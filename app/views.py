08e89dbce30d57286d71275afbdb4548b3fead7e
Savinay Narendra <savinay.90@gmail.com>
2015-01-29 16:23:54 +0530

    contact request charts added

--- app/views.py
+++ app/views.py
@@ -43,15 +43,19 @@ def bar(request):
         data={}
         data['activeusers'] = []
         data['conversion'] = []
-        collection_active_users = db.overallHOP.find({'timeStamp': {'$gte': yutcoid, '$lte': tutcoid}, 'chart':'Active_Users'},{"_id":0, "timeStamp": 0})
-        collection_conversion = db.overallHOP.find({'timeStamp': {'$gte': yutcoid, '$lte': tutcoid}, 'chart':'Conversion'},{"_id":0, "timeStamp": 0})
+        data['contact_requests'] = []
+        collection_active_users = db.properData.find({'timeStamp': {'$gte': yutcoid, '$lte': tutcoid}, 'chart':'Active_Users'},{"_id":0, "timeStamp": 0})
+        collection_conversion = db.properData.find({'timeStamp': {'$gte': yutcoid, '$lte': tutcoid}, 'chart':'Conversion'},{"_id":0, "timeStamp": 0})
+        collection_contact_requests = db.properData.find({'timeStamp': {'$gte': yutcoid, '$lte': tutcoid}, 'chart':'Contact Requests'},{"_id":0, "timeStamp": 0})
         # print strftime("%Y%m%d", y), strftime("%Y%m%d", t)
-        print collection_conversion, collection_active_users
+        print collection_conversion, collection_active_users, collection_contact_requests
         for obj in collection_active_users:
             print obj
             data['activeusers']+=[obj]
         for obj in collection_conversion:
             data['conversion']+=[obj]
+        for obj in collection_contact_requests:
+            data['contact_requests']+=[obj]
         print data
         return HttpResponse(json.dumps(data), content_type="application/json")
 
--- overallHOP/static/js/hopapp.js
+++ overallHOP/static/js/hopapp.js
@@ -60,6 +60,29 @@ $(function() {
         });
     }
 
+
+    var generateContactRequestsLineChart = function (data) {
+            var num_users_array = [];
+            num_users_array.push('Contact Requests');
+            var num;
+            for (var i = data.length - 1; i >= 0; i--) {
+                console.log(data[i].num_users);
+                num = parseInt(data[i].num_users, 10);
+                num_users_array.push(num);
+            };
+
+            var chart = c3.generate({
+                bindto: '#chartContactRequests',
+                data: {
+                    columns: [
+                        num_users_array
+                    ]
+                }
+            });
+        }
+
+
+
     var generateActiveUsersDonutChart = function (data) {
         var organicChannel = [];
         var socialChannel = [];
@@ -185,6 +208,72 @@ $(function() {
     }
 
 
+    var generateContactRequestsDonutChart = function (data) {
+        var organicChannel = [];
+        var socialChannel = [];
+        var emailChannel = [];
+        var directChannel = [];
+        var paidChannel = [];
+        var organic = 0;
+        var social = 0;
+        var email = 0;
+        var direct = 0;
+        var paid = 0;
+
+        organicChannel.push('organic');
+        socialChannel.push('social');
+        emailChannel.push('email');
+        directChannel.push('direct');
+        paidChannel.push('paid');
+
+        for (var i = data.length - 1; i >= 0; i--) {
+            organic = organic + data[i].channels.organic;
+            social = social + data[i].channels.social;
+            email = email + data[i].channels.email;
+            direct = direct + data[i].channels.direct;
+            paid = paid + data[i].channels.paid;
+        };
+
+        console.log(organic, social, email, direct, paid);
+
+        organicChannel.push(organic);
+        socialChannel.push(social);
+        emailChannel.push(email);
+        directChannel.push(direct);
+        paidChannel.push(paid);
+
+        var donutChart = c3.generate({
+            bindto: '#donutChartContactRequests',
+            data: {
+                columns: [
+                    organicChannel,
+                    socialChannel,
+                    emailChannel,
+                    directChannel,
+                    paidChannel,
+                ],
+                type: 'donut',
+                onclick: function(d, i) {
+                    console.log("onclick", d, i);
+                },
+                onmouseover: function(d, i) {
+                    console.log("onmouseover", d, i);
+                },
+                onmouseout: function(d, i) {
+                    console.log("onmouseout", d, i);
+                }
+            },
+            donut: {
+                title: "Channel-wise Contact Requests"
+            }
+        });
+    }
+
+
+
+
+
+
     var today = new Date();
     today = today.getDate() + '/' + (today.getMonth() + 1) + '/' +  today.getFullYear()
     console.log(today);
@@ -208,7 +297,9 @@ $(function() {
                 generateActiveUsersLineChart(data.activeusers);
                 generateActiveUsersDonutChart(data.activeusers);
                 generateConversionLineChart(data);
-                generateConversionDonutChart(data.conversion)
+                generateConversionDonutChart(data.conversion);
+                generateContactRequestsLineChart(data.contact_requests);
+                generateContactRequestsDonutChart(data.contact_requests);
             },
             error: function(err) {
                 console.log("error: ", err);
